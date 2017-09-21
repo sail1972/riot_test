36b141e3e383a70428233b6804ae44b9dce5e60b
Sail Lee <sail1972@gmail.com>
2017-09-22 02:15:58 +0800

    Project init.

---
+++ README.md
@@ -0,0 +1,6 @@
+#A test of Rails with Riot.js#
+
+This is a test for using Rails with Riot.js.
+
+* Rails v3.2.13
+* Riot.js v3.7.0
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RiotTest::Application.config.secret_token = '1ccaecc9001f655d2d9d32cf848f9ad55fa3440eec3cf2500851cfef0704651d7dd08b0f9e9fc9a21aeca4e2106215dc3bd747fd9a0c5807085e647ca823fc33'
